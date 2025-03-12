27b682af716495308c03d11e5260d2ee74d4d105
pouriashiralipour <pouriashiralipour@gmail.com>
2025-03-12 14:59:45 +0330

    add products contetx

---
+++ src/context/ProductContext.jsx
@@ -0,0 +1,26 @@
+import { Children, createContext, useEffect, useState } from "react";
+import api from "../services/config";
+
+const ProductContext = createContext();
+
+const ProductsProvider = ({ children }) => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        setProducts(await api.get("/products"));
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchProducts;
+  }, []);
+  return (
+    <ProductContext.Provider value={products}>
+      {children}
+    </ProductContext.Provider>
+  );
+};
+
+export default ProductsProvider;
