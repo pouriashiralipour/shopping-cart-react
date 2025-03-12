f330b162abbf2e71b8ea0cb7448b26f8c5a2b558
pouriashiralipour <pouriashiralipour@gmail.com>
2025-03-12 20:36:08 +0330

    add search tag to products page

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,22 +1,45 @@
+import { useState } from "react";
+import { ImSearch } from "react-icons/im";
+
 import Card from "../components/Card";
 import Loader from "../components/Loader";
 import { useProducts } from "../context/ProductContext";
-
 import styles from "./ProductPage.module.css";
 
 const ProductPage = () => {
   const products = useProducts();
 
+  const [search, setSearch] = useState("");
+
+  const searchHandler = () => {
+    console.log(search);
+  };
   return (
-    <div className={styles.container}>
-      <div className={styles.products}>
-        {!products.length && <Loader />}
-        {products.map((product) => (
-          <Card key={product.id} data={product} />
-        ))}
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="Search"
+          name="search"
+          value={search}
+          onChange={(event) =>
+            setSearch(event.target.value.toLowerCase().trim())
+          }
+        />
+        <button onClick={searchHandler}>
+          <ImSearch />
+        </button>
       </div>
-      <div>sidebar</div>
-    </div>
+      <div className={styles.container}>
+        <div className={styles.products}>
+          {!products.length && <Loader />}
+          {products.map((product) => (
+            <Card key={product.id} data={product} />
+          ))}
+        </div>
+        <div>sidebar</div>
+      </div>
+    </>
   );
 };
 
