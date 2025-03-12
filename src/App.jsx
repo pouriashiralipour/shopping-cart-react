d0df215e74d1d3d1e682994f3cf975e42c40aca8
pouriashiralipour <pouriashiralipour@gmail.com>
2025-03-12 14:39:43 +0330

    add routing of project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { Navigate, Route, Routes } from "react-router";
+import ProductPage from "./pages/ProductPage";
+import ProductDetailsPage from "./pages/ProductDetailsPage";
+import CheckoutPage from "./pages/CheckoutPage";
+import PageNotFound from "./pages/404";
+
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route index element={<Navigate to="/products" replace />} />
+      <Route path="/products" element={<ProductPage />} />
+      <Route path="/products/:id" element={<ProductDetailsPage />} />
+      <Route path="/checkout" element={<CheckoutPage />} />
+      <Route path="*" element={<PageNotFound />} />
+    </Routes>
+  );
 }
 
 export default App;
